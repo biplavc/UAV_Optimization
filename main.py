99652cd7bea9c6e1d5f0a4b605a5e6f522c2a020
Biplav Choudhury <biplav93@gmail.com>
2020-04-22 20:13:37 -0400

    distance func added

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ random.seed(42)
 def initialize_area():
     x_vals = random.sample(range(1, L-1), I)
     y_vals = random.sample(range(1, B-1), I)
+    z_vals = [0]*I
 
-    coordinates = list(zip(x_vals,y_vals))
-    plt.scatter(*zip(*coordinates))
+    coordinates = list(zip(x_vals,y_vals,z_vals))
+    # plt.scatter(*zip(*coordinates))
+    plt.scatter(x_vals, y_vals)
+    # print(coordinates)
+    plt.xlabel("X-axis (m)")
+    plt.ylabel("Y-axis (m)")
+    plt.title('Simulation Area')
     plt.show()
-    
+
 if __name__ == '__main__':
     initialize_area()
 
